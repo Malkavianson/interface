2876690c4641fd9895440670ba28a932fd83fefb
Malkavianson <alexneto.best@hotmail.com>
2023-05-26 10:07:46 -0300

    feat: Prisma connection

--- src/pages/api/produtos.ts
+++ src/pages/api/produtos.ts
@@ -1,113 +1,82 @@
-// import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-// import prisma from '../../path/to/prisma/client';
-// import cors from 'cors';
-// import swaggerUi from 'swagger-ui-express';
+import { prisma } from "@/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-// // Configuração do CORS
-// const corsOptions = {
-//   origin: '*', // Defina a origem permitida para sua aplicação (ex: 'http://localhost:3000')
-// };
+// import swaggerUi from "swagger-ui-express";
 
-// // Configuração do Swagger
+// Configuração do Swagger
 // const swaggerOptions = {
-//   swaggerDefinition: {
-//     info: {
-//       title: 'API de Produtos',
-//       version: '1.0.0',
-//     },
-//   },
-//   apis: ['pages/api/product.ts'],
+// 	swaggerDefinition: {
+// 		info: {
+// 			title: "API de Produtos",
+// 			version: "1.0.0",
+// 		},
+// 	},
+// 	apis: ["pages/api/product.ts"],
 // };
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) {
-//   // Aplica o middleware de CORS para permitir requisições de diferentes origens
-//   cors(corsOptions)(req, res, (err)=>{
-//     return console.log(err)
-//   });
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	if (req.method === "GET") {
+		try {
+			const products = await prisma.product.findMany();
+			res.status(200).json(products);
+		} catch (error) {
+			res.status(500).json({ error: "Erro ao obter os produtos." });
+		}
+	}
 
-//   // Rota GET para obter todos os produtos
-//   if (req.method === 'GET') {
-//     try {
-//       const products = await prisma.product.findMany();
-//       res.status(200).json(products);
-//     } catch (error) {
-//       res.status(500).json({ error: 'Erro ao obter os produtos.' });
-//     }
-//   }
+	// Rota POST para criar um novo produto
+	if (req.method === "POST") {
+		try {
+			const { name, price } = req.body;
 
-//   // Rota POST para criar um novo produto
-//   if (req.method === 'POST') {
-//     try {
-//       const { name, price } = req.body;
+			const createdProduct = await prisma.product.create({
+				data: {
+					name,
+					price,
+				},
+			});
 
-//       const createdProduct = await prisma.product.create({
-//         data: {
-//           name,
-//           price,
-//         },
-//       });
+			res.status(201).json(createdProduct);
+		} catch (error) {
+			res.status(500).json({ error: "Erro ao criar o produto." });
+		}
+	}
 
-//       res.status(201).json(createdProduct);
-//     } catch (error) {
-//       res.status(500).json({ error: 'Erro ao criar o produto.' });
-//     }
-//   }
+	// Rota PATCH para atualizar um produto existente
+	if (req.method === "PATCH") {
+		try {
+			const { id, name, price } = req.body;
 
-//   // Rota PATCH para atualizar um produto existente
-//   if (req.method === 'PATCH') {
-//     try {
-//       const { id, name, price } = req.body;
+			const updatedProduct = await prisma.product.update({
+				where: { id },
+				data: {
+					name,
+					price,
+				},
+			});
 
-//       const updatedProduct = await prisma.product.update({
-//         where: { id },
-//         data: {
-//           name,
-//           price,
-//         },
-//       });
+			res.status(200).json(updatedProduct);
+		} catch (error) {
+			res.status(500).json({ error: "Erro ao atualizar o produto." });
+		}
+	}
 
-//       res.status(200).json(updatedProduct);
-//     } catch (error) {
-//       res.status(500).json({ error: 'Erro ao atualizar o produto.' });
-//     }
-//   }
-
-//   // Rota DELETE para excluir um produto
-// //   if (req.method === 'DELETE') {
-// //     try {
-// //       const { id } = req.body;
-
-// //       await prisma.product.delete({
-// //         where: { id },
-// //       });
-
-// //       res.status(204).end();
-// //     } catch (error) {
-// //       res.status(500).json({ error: 'Erro ao
-
-// //     })}}}
-
-//       // Rota GET para obter um produto pelo ID
-//   if (req.method === 'GET') {
-//     try {
-//       const { id } = req.query;
-
-//       const product = await prisma.product.findUnique({
-//         where: { id: Number(id) },
-//       });
-
-//       if (!product) {
-//         res.status(404).json({ error: 'Produto não encontrado.' });
-//       } else {
-//         res.status(200).json(product);
-//       }
-//     } catch (error) {
-//       res.status(500).json({ error: 'Erro ao obter o produto.' });
-//     }
-//   }
-
-//   // Configuração do Swagger
-//   if (req.url?.startsWith('/api-docs')) {
-//     swaggerUi.setup(swaggerOptions)(req, res);
-//   }
-// }
+	if (req.method === "DELETE") {
+		const { id } = req.body;
+		try {
+			const products = await prisma.product.delete({
+				where: { id },
+			});
+			res.status(200).json(products);
+		} catch (error) {
+			res.status(500).json({ error: "Erro ao obter os produtos." });
+		}
+	}
+	// Configuração do Swagger
+	// if (req.url?.startsWith("/api-docs")) {
+	// 	swaggerUi.setup(swaggerOptions)(req, res);
+	// }
+}
---
+++ src/types/produtos/interfaces.ts
@@ -0,0 +1,50 @@
+export interface IProduto {
+    descricao: string;
+    disponibilidade: Date;
+    especie: string;
+    genero: EGeneroProduto;
+    imagens: string[];
+    // url da imagem salvas em algum banco
+    nome: string;
+    portador: EPortadorCNPJ; // Pode se tornar relacionado ao id do cnpj/user
+    porte: EPorteProduto;
+    preco: number; // procurar salvar inteiros e colocar os centavos manualmente ex: 549850 => 5498,50
+    referencia: string;
+    status: EStatusProduto;
+    visibilidade: EVisibilidadeProduto;
+    tipo: ETipoProduto;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export enum EPortadorCNPJ {
+    JiboiasBrasil,
+    AnimaisBrasil
+}
+
+export enum EVisibilidadeProduto {
+    JiboiasBrasil,
+    AnimaisBrasil
+}
+
+export enum EPorteProduto {
+    Adulto,
+    Filhote
+}
+
+export enum EGeneroProduto {
+    Femea,
+    Macho
+}
+
+export enum EStatusProduto {
+    Disponivel,
+    Vendido
+}
+
+export enum ETipoProduto {
+    Jiboia,
+    JiboiaArcoIris,
+    Piton,
+    Iguana
+}
